0d797238c4e101dd2887e7a62fd9ba1e351564cc
alexandru1199 <35688560+alexandru1199@users.noreply.github.com>
2021-07-01 20:40:28 +0300

    Database

--- src/main/java/Bot_Main.java
+++ src/main/java/Bot_Main.java
@@ -1,19 +1,20 @@
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
 
-
-import net.dv8tion.jda.api.utils.cache.CacheFlag;
-
-import javax.security.auth.login.LoginException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Bot_Main {
-    public static void main(String[] args)
+    public static void main(String[] args) throws  java.sql.SQLException
             {
-                Database.initialize_database();
+                Statement statement=Database.initialize_database();
+                statement.executeUpdate("DELETE FROM `discord`.`discord_users` WHERE NAME='Viorel'");
+                statement.executeUpdate("DELETE FROM `discord`.`discord_users` WHERE NAME='Giorel'");
+                statement.executeUpdate("INSERT INTO `discord`.`discord_users`VALUES('2','Viorel','lis',2000);");
+                statement.executeUpdate("INSERT INTO `discord`.`discord_users`VALUES('3','Giorel','lis',2000);");
+                ResultSet resultSet = statement.executeQuery("select * from discord_users");
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("Name"));
+                }
         try {
 
             JDABuilder builder = Building.Build(args[0]);
--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -3,18 +3,15 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class Database {
-    public static void initialize_database() {
-        try {
+public class Database  {
+
+    public static Statement initialize_database() throws  java.sql.SQLException {
+
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/discord", "root", "FHAL4Gen");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from discord_users");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("Name"));
-            }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            return  statement;
+
+
     }
 }
